954dbe224fdef97cf32d5a80d5843c1862ac5573
Diogo Braz <diogo.braz04@gmail.com>
2024-01-08 00:42:05 -0300

    fix eslint

--- src/domain/use-cases/answer-question.spec.ts
+++ src/domain/use-cases/answer-question.spec.ts
@@ -1,21 +1,20 @@
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswerRepository } from "@/domain/repositories/answers-repository";
-import { Answer } from "@/domain/entities/answer";
+import { AnswerQuestionUseCase } from './answer-question'
+import { AnswerRepository } from '@/domain/repositories/answers-repository'
+import { Answer } from '@/domain/entities/answer'
 
 const fakeAnswerRepository: AnswerRepository = {
-  create: async (answer: Answer) => {
-    return;
-  }
-};
+  // eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+  create: async (answer: Answer) => {},
+}
 
 test('create an answer', async () => {
-  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository);
+  const answerQuestion = new AnswerQuestionUseCase(fakeAnswerRepository)
 
   const answer = await answerQuestion.execute({
     instructorId: '1',
     questionId: '1',
     content: 'Nova resposta',
-  });
+  })
 
-  expect(answer.content).toBe('Nova resposta');
+  expect(answer.content).toBe('Nova resposta')
 })
